e4fdd0e6242bf54fa03ff32b8f91fdbadb46f2f8
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-05-11 15:46:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$panda = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0cc25e2e-d94d-42eb-a44c-a0dbb83598ae/d1x0kxu-81c4653a-fd15-441a-955f-0c9a36f3deea.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMGNjMjVlMmUtZDk0ZC00MmViLWE0NGMtYTBkYmI4MzU5OGFlXC9kMXgwa3h1LTgxYzQ2NTNhLWZkMTUtNDQxYS05NTVmLTBjOWEzNmYzZGVlYS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.l1Y-AJt3hRs0gIaHkE6TQAu9o1ecUacdlGr6dCLOLfk"
+$panda = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0cc25e2e-d94d-42eb-a44c-a0dbb83598ae/d1x0kxu-81c4653a-fd15-441a-955f-0c9a36f3deea.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMGNjMjVlMmUtZDk0ZC00MmViLWE0NGMtYTBkYmI4MzU5OGFlXC9kMXgwa3h1LTgxYzQ2NTNhLWZkMTUtNDQxYS05NTVmLTBjOWEzNmYzZGVlYS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.l1Y-AJt3hRs0gIaHkE6TQAu9o1ecUacdlGr6dCLOLfk";
 define ('MAX_MINION', 50);
 define ('MIN_MINION', 10);
 $pocetpand = rand(MIN_MINION, MAX_MINION);
